8987622aecc9b62234fefa272b3b84455d3a0dea
mat barb <mattia.barbaresi@gmail.com>
2022-02-07 19:46:28 +0100

    multi_integration

--- classes/multi_integration.py
+++ classes/multi_integration.py
@@ -11,18 +11,20 @@ class MIModule:
     def encode(self, *percepts):
         """
         Encode associations (Hebb) between percepts
-        :param percepts: list of list of units for each dimension/sensor/modality
+        :param percepts list of units for each dimension/sensor/modality
         """
         # add weight for each pair
         for n in range(len(percepts)-1):  # for each feature/dimension
             for i in range(len(percepts[n])):  # for each unit in that dimension
                 for m in range(n,len(percepts)):  # for all others dimensions
                     for j in range(len(percepts[m])):  # for each unit in others dimensions
-                        if self.G.has_edge(percepts[i], percepts[j]):
-                            self.G[i][j]['weight'] += 1
+                        if self.G.has_edge(percepts[n][i], percepts[m][j]):
+                            self.G[percepts[n][i]][percepts[m][j]]['weight'] += 1
                         else:
-                            self.G.add_edge(percepts[i], percepts[j], weight=1)
+                            self.G.add_edge(percepts[n][i], percepts[m][j], weight=1)
 
     def draw_graph(self):
-        nx.draw(self.G)
+        nx.draw_networkx(self.G)
+
         plt.draw()
+        plt.show()
--- main.py
+++ main.py
@@ -8,13 +8,16 @@ from classes.pparser import ParserModule
 from classes.tps import TPSModule
 import numpy as np
 
+from computation import ComputeModule
+from multi_integration import MIModule
+
 np.set_printoptions(linewidth=np.inf)
 rng = np.random.default_rng(const.RND_SEED)
 
 # file_names = \
 #     ["input", "input2", "saffran", "thompson_newport", "reber", "all_songs_in_G", "all_irish-notes_and_durations"]
 
-file_names = ["input"]
+file_names = ["bicinia"]
 
 # maintaining INTERFERENCES/FORGETS separation by a factor of 10
 interferences = [0.005]
@@ -28,7 +31,7 @@ for order in tps_orders:
         for fogs in forgets:
             for t_mem in thresholds_mem:
                 # init
-                root_dir = const.OUT_DIR + "{}_{}_({}_{}_{})_{}/"\
+                root_dir = const.OUT_DIR + "{}_{}_({}_{}_{})_{}/" \
                     .format(method, order, t_mem, fogs, interf, time.strftime("%Y%m%d-%H%M%S"))
                 # root_dir = const.OUT_DIR + "RND_(2-3)_({}_{}_{})_{}/".\
                 #     format(t_mem, fogs, interf, time.strftime("%Y%m%d-%H%M%S"))
@@ -63,72 +66,26 @@ for order in tps_orders:
                         # sequences = utils.load_irish_n_d_repeated("data/all_irish-notes_and_durations-abc.txt")
                         sequences = utils.load_irish_n_d("data/all_irish-notes_and_durations-abc.txt")
                     elif fn == "bicinia":
-                        sequences = utils.load_bicinia_single("data/bicinia/", seq_n=2)
+                        sequences = utils.load_bicinia_full("data/bicinia/")
                     else:
                         with open("data/{}.txt".format(fn), "r") as fp:
                             # split lines char by char
                             sequences = [list(line.strip()) for line in fp]
 
                     # read percepts using parser function
-                    actions = []
-                    initial_set = set()
                     start_time = datetime.now()
-                    for iter,(s1,s2) in enumerate(zip(sequences)):
-                        old_p = ""
-                        while len(s1) > 0:
-                            print(" ------------------------------------------------------ ")
-                            # read percept as an array of units
-                            # active elements in mem shape perception
-                            active_mem = dict((k, v) for k, v in pars.mem.items() if v >= const.MEM_THRES)
-                            # active_mem = dict((k, v) for k, v in pars.mem.items() if v >= 0.5)
-                            units, action = utils.read_percept(rng, active_mem, s1, ulens=units_len, tps=tps_1)
-                            actions.append(action)
-                            p = "".join(units)
-                            tps_1.encode(old_p + p)
-                            # save past for tps
-                            old_p = p[-tps_order:]
-                            print("units: ", units, " -> ", p)
-                            # add entire percept
-                            if len(p) <= max(units_len):
-                                # p is a unit, a primitive
-                                if p in pars.mem:
-                                    pars.mem[p] += const.WEIGHT / 2
-                                else:
-                                    pars.mem[p] = const.WEIGHT
-                            else:
-                                tps_units.encode(units)
-                                pars.add_weight(p, comps=units, weight=const.WEIGHT)
-                            # forgetting and interference
-                            pars.forget_interf(rng, p, comps=units, forget=const.FORGETTING, interfer=const.INTERFERENCE, ulens=units_len)
-                            tps_units.forget(units, forget=const.FORGETTING)
-                            s = s[len(p):]
-
-                # dc = fc.distributional_context(fc_seqs, 3)
-                # # print("---- dc ---- ")
-                # # pp.pprint(dc)
-                # classes = fc.form_classes(dc)
-                # class_patt = fc.classes_patterns(classes, fc_seqs)
-
-                # normilizes memories
-                tps_1.normalize()
-                tps_units.normalize()
-
-                # generate sample sequences
-                decoded = []
-                gens = tps_units.generate_new_sequences(rng, min_len=100)
-                print("gens: ", gens)
-
-
-                # save all
-                with open(out_dir + "action.json", "w") as of:
-                    json.dump(actions,of)
-                utils.plot_actions(actions, path=out_dir)
-
-                # print(tps_units.mem)
-                # utils.plot_gra(tps_units.mem)
-                utils.plot_gra_from_normalized(tps_units.norm_mem, filename=out_dir + "tps_units", be=base_encoder)
-                utils.plot_gra_from_normalized(tps_1.norm_mem,  filename=out_dir + "tps_1", be=base_encoder)
-                # plot memeory chunks
-                # for "bicinia" and "all_irish_notes_and_durations" use base_decode
-                o_mem = dict(sorted([(base_encoder.base_decode(x),y) for x,y in pars.mem.items()], key=lambda it: it[1], reverse=True))
-                utils.plot_mem(o_mem, out_dir + "words_plot.png", save_fig=True, show_fig=True)
+                    mim = MIModule()
+                    cm1 = ComputeModule(rng, order=order, weight=const.WEIGHT, interference=interf, forgetting=fogs,
+                                        memory_thres=t_mem, unit_len=const.ULENS, method=method)
+                    cm2 = ComputeModule(rng, order=order, weight=const.WEIGHT, interference=interf, forgetting=fogs,
+                                        memory_thres=t_mem, unit_len=const.ULENS, method=method)
+                    for iter, (s1, s2) in enumerate(zip(sequences[0], sequences[1])):
+                        first_in_seq = True
+                        while len(s1) > 0 and len(s2) > 0:
+                            p1, units1, _ = cm1.compute(s1, first_in_seq)
+                            s1 = s1[len(p1.strip().split(" ")):]
+                            p2, units2, _ = cm2.compute(s2, first_in_seq)
+                            s2 = s2[len(p2.strip().split(" ")):]
+                            first_in_seq = False
+                            mim.encode(units1,units2)
+                    mim.draw_graph()
