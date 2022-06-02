9c45657ebff2bfcd5c0166723cf10de2e8ce297c
mat barb <mattia.barbaresi@gmail.com>
2022-06-02 18:49:48 +0200

    creativity

--- CTPs/creativity.py
+++ CTPs/creativity.py
@@ -1,17 +1,8 @@
 """Module for creativity classes and functions"""
 
-# digraph {
-# 	START [label="START (1.549)"]
-# 	START -> d [label=0.362 penwidth=1.0851063829787235]
-# 	START -> k [label=0.234 penwidth=0.7021276595744681]
-# 	START -> m [label=0.404 penwidth=1.2127659574468084]
-# 	a [label="a (1.564)"]
-# 	a -> f [label=0.383 penwidth=1.148936170212766]
-# 	a -> l [label=0.255 penwidth=0.7659574468085106]
-# 	a -> z [label=0.362 penwidth=1.0851063829787235]
-# 	b [label="b (1.531)"]
-# }
+import more_itertools as mit
 import utils
+from difflib import SequenceMatcher
 
 
 def creative_gens(rand_gen, kg0, n_seq=10, min_len=30):
@@ -22,7 +13,8 @@ def creative_gens(rand_gen, kg0, n_seq=10, min_len=30):
         # tot = sum([float(x[2]["label"]) for x in kg0.edges(data=True) if x[0] in kg0.successors("START")])
         for x, y, v in kg0.edges("START", data=True):
             init_keys.append(y)
-            init_values.append(float(v["label"]))
+            c = (1 - float(v["p"])) * float(v["u"]) * (0.67 - float(v["v"]))
+            init_values.append(c)
     else:
         print("no START found")
 
@@ -31,8 +23,9 @@ def creative_gens(rand_gen, kg0, n_seq=10, min_len=30):
         return res
 
     for _ in range(n_seq):
-        seq = init_keys[utils.mc_choice(rand_gen, init_values)]  # choose rnd starting point (monte carlo)
-        _s = seq
+        seq = []
+        _s = init_keys[utils.mc_choice(rand_gen, init_values)]  # choose rnd starting point (monte carlo)
+        seq.append(_s)
         for _ in range(min_len):
             # succs = list(kg0.edges(_s, data=True))
             # _s = succs[utils.mc_choice(rand_gen, succs)]
@@ -40,11 +33,61 @@ def creative_gens(rand_gen, kg0, n_seq=10, min_len=30):
             succs_values = []
             for x, y, v in kg0.edges(_s, data=True):
                 succs.append(y)
-                succs_values.append(float(v["label"]))
+                succs_values.append(float(v["p"]))
             if succs:
                 _s = succs[utils.mc_choice(rand_gen, succs_values)]
                 if _s != "END":
-                    seq += " " + _s
+                    seq.append(_s)
         res.append(seq)
 
     return res
+
+
+def evaluate(seqs):
+    vals = []
+    for seq in seqs:
+        sseq = "".join([_.replace(" ", "") for _ in seq])
+        # vals.append((seq, float(input("sequence:" + " ".join(seq) + ". Please judge:"))))
+        val = 0
+        if "ddcA" in sseq:
+            val += 1
+        if "cec" in sseq:
+            val += 1
+        if "GBGAGBGA" in sseq:
+            val += 2
+        if "BBAGEED" in sseq:
+            val += 2
+        if "DF" in sseq:
+            val += 1
+        val /= 5
+
+        vals.append((seq,val))
+
+    return vals
+
+
+def evaluate_similarity(seqs, rep):
+    vals = []
+    for seq in seqs:
+        sseq = "".join([_.replace(" ", "") for _ in seq])
+        val = rep_similarity(sseq, rep)
+        vals.append((seq,val))
+
+    return vals
+
+
+def rep_similarity(seq, rep):
+    val = 0
+    for _ in rep:
+        val += SequenceMatcher(None, seq, _).ratio()
+    return val / len(rep)  # mean similarity
+
+
+def update(g_evals, G):
+    for seq, val in g_evals:
+        for sn,en in mit.pairwise(seq):
+            u = float(G[sn][en]["u"]) if "u" in G[sn][en] else 0
+            v = float(G[sn][en]["v"]) if "v" in G[sn][en] else 0
+            G[sn][en]["u"] = (u + val)/2  # u average (u mean)
+            G[sn][en]["v"] = (v + abs(u - val))/2  # v average (u variability)
+    return G
