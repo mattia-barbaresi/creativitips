fixed tps, graphs and computation: now it works well!

creativitips/tps_main.py
@@ -3,6 +3,8 @@ import json
 import os
 import numpy as np
 from matplotlib import pyplot as plt
+
+import complexity
 from creativitips import utils
 from creativitips import const
 from creativitips.CTPs.computation import Computation
@@ -11,12 +13,50 @@ from creativitips.CTPs.graphs import TPsGraph
 
 # NOTES: more iterations over the same input enhance resulting model!
 
-def elaborate(test=False):
-    if test:
-        print("Called creativitips elaborate!")
+def elaborate(file_in="", dir_out=""):
+    print("processing {} series ...".format(file_in))
+    # init
+    res = dict()
+    rnd_gen = np.random.default_rng(const.RND_SEED)
+    rout = dir_out + "/tps_results/"
+    os.makedirs(rout, exist_ok=True)
+    with open(file_in, "r") as fp:
+        seqs = [list(line.strip().split(" ")) for line in fp]
+    cm_m = Computation(rnd_gen)
+
+    for iter, s in enumerate(seqs):
+        fis = True
+        while len(s) > 0:
+            # compute next percept
+            p, units = cm_m.compute(s, first_in_seq=fis)
+            fis = False
+            # update s
+            s = s[len(p.strip().split(" ")):]
+        cm_m.compute_last()
+
+    cm_m.tps_units.normalize()
+    res[iteration] = dict()
+    res[iteration]["generated"] = cm_m.tps_units.generate_new_seqs(rng)
+    im = dict(sorted([(x, y) for x, y in cm_m.pars.mem.items()],
+                     key=lambda it: it[1], reverse=True))
+    res[iteration]["mem"] = im
+
+    res["processing time"] = str((datetime.now() - start_time).total_seconds())
+    # calculate states uncertainty
+    cm_m.tps_1.compute_states_entropy()
+    cm_m.tps_units.compute_states_entropy()
+    # generalization
+    cm_m.generalize(rout)
+    # save result
+    with open(rout + "res.json", "w") as of:
+        json.dump(res, of)
+    # save generated
+    with open(rout + "generated.json", "w") as of:
+        json.dump(gens, of)
 
 
 if __name__ == "__main__":
+
     np.set_printoptions(linewidth=np.inf)
     rng = np.random.default_rng(const.RND_SEED)
 
@@ -24,14 +64,15 @@ if __name__ == "__main__":
     #     ["isaac", "input", "input2", "saffran", "thompson_newport", "reber", "all_songs_in_G",
     #     "all_irish-notes_and_durations","cello", "bach_compact"]
 
-    file_names = ["mediapipe2"]
+    file_names = ["input", "input2","Onnis2003_L1_24","Onnis2003_L2_24", "saffran"]
+    file_names = ["input"]
 
     # maintaining INTERFERENCES/FORGETS separation by a factor of 10
-    thresholds_mem = [0.9]
+    thresholds_mem = [1.0]
     interferences = [0.005]
     forgets = [0.05]
-    tps_orders = [1]
-    methods = ["CT"]  # MI, CT or BRENT
+    tps_orders = [2]
+    methods = ["FTP"]  # MI, CT or BRENT, FTP
 
     for tps_method in methods:
         for tps_order in tps_orders:
@@ -65,7 +106,6 @@ if __name__ == "__main__":
                                 raise
 
                             results = dict()
-
                             # --------------- INPUT ---------------
                             sequences = utils.read_sequences(rng, fn)
 
@@ -74,8 +114,7 @@ if __name__ == "__main__":
 
                             # init module for computation
                             cm = Computation(rng, order=tps_order, weight=const.WEIGHT, interference=interf,
-                                             forgetting=fogt,
-                                             mem_thres=t_mem, unit_len=const.ULENS, method=tps_method)
+                                             forgetting=fogt, mem_thres=t_mem, unit_len=const.ULENS, method=tps_method)
 
                             for iteration, s in enumerate(sequences):
                                 fis = True
@@ -87,14 +126,11 @@ if __name__ == "__main__":
                                     # update s
                                     s = s[len(p.strip().split(" ")):]
                                 cm.compute_last()
+
                                 # --------------- GENERATE ---------------
-                                if iteration % 5 == 1:
-                                    cm.tps_units.normalize()
-                                    results[iteration] = dict()
-                                    results[iteration]["generated"] = cm.tps_units.generate_new_seqs(rng, min_len=100)
-                                    im = dict(sorted([(x, y) for x, y in cm.pars.mem.items()],
-                                                     key=lambda it: it[1], reverse=True))
-                                    results[iteration]["mem"] = im
+                                # if iteration % 5 == 1:
+                                #     results[iteration] = dict()
+                                #     utils.generate_seqs(rng, cm, results[iteration])
 
                             # class form on graph
                             # dc = fc.distributional_context(fc_seqs, 3)
@@ -108,7 +144,6 @@ if __name__ == "__main__":
                             # normalizes memories
                             cm.tps_1.normalize()
                             cm.tps_units.normalize()
-
                             # embedding chunks
                             # emb_le = preprocessing.LabelEncoder()
                             # emb_le.fit(list(cm.tps_units.le_rows.classes_) + list(cm.tps_units.le_cols.classes_))
@@ -128,31 +163,24 @@ if __name__ == "__main__":
                             # calculate states uncertainty
                             cm.tps_1.compute_states_entropy()
                             cm.tps_units.compute_states_entropy()
+
                             # generalization
+                            # if input is a single array (as original saffran) the next command loops forever
+                            # for the presence of cycles in the graph
                             cm.generalize(fi_dir)
+
                             # generate sample sequences
                             gens = cm.tps_units.generate_new_seqs(rng, min_len=100)
                             print("gens: ", gens)
-                            fig, axs = plt.subplots(3)
-                            axs[0].set_title("ftps")
-                            axs[1].set_title("mis")
-                            axs[2].axis('off')
-                            axs[2].axis("tight")
-                            ll = 0
-                            for gg in gens:
-                                x1 = cm.tps_1.get_ftps_sequence(gg.split(" "))
-                                x2 = cm.tps_1.get_mis_sequence(gg.split(" "))
-                                axs[0].plot(x1)
-                                axs[1].plot(x2)
-                                axs[2].set_xlim(axs[0].get_xlim())
-                                axs[2].set_ylim([0, 40])
-                                for i, x in enumerate(gg.split(" ")[cm.tps_1.order:]):
-                                    axs[2].text(i, ll, '{}'.format(x))
-                                    # axs[1].text(i, ll, '{}'.format(x))
-                                ll += 2
-                            # plt.show()
-                            plt.savefig(fi_dir + "tps_plot.png", bbox_inches='tight')
-                            plt.close('all')
+                            # plot tps
+                            utils.plot_tps_sequences(cm, [" ".join(x) for x in sequences[:20]], fi_dir)
+                            comp_res = complexity.calculate_complexities(gens)
+                            results["complexities"] = comp_res
+                            print("---------- complexities:")
+                            for it, vl in comp_res.items():
+                                print(it, vl)
+                            print("----------")
+
                             # print("REGENERATION")
                             # for g in gens:
                             #     print(cm.tps_1.get_units_brent(g))
@@ -169,18 +197,21 @@ if __name__ == "__main__":
                             utils.plot_actions(cm.actions, path=fi_dir, show_fig=False)
                             # print(tps_units.mem)
                             # utils.plot_gra(tps_units.mem)
+
                             print("plotting tps units...")
                             with open(fi_dir + "tps_units.json", "w") as of:
                                 json.dump(cm.tps_units.mem, of)
                             utils.plot_gra_from_normalized(cm.tps_units, filename=fi_dir + "tps_units", render=True)
+
                             print("plotting tps all...")
                             with open(fi_dir + "tps.json", "w") as of:
                                 json.dump(cm.tps_1.mem, of)
                             utils.plot_gra_from_normalized(cm.tps_1, filename=fi_dir + "tps_symbols", render=True)
+
                             print("plotting memory...")
                             # plot memory chunks
-                            om = dict(
-                                sorted([(x, y) for x, y in cm.pars.mem.items()], key=lambda it: it[1], reverse=True))
+                            om = dict(sorted([(x, y) for x, y in cm.pars.mem.items()][:30], key=lambda _i: _i[1],
+                                             reverse=True))
                             utils.plot_mem(om, fi_dir + "words_plot.png", save_fig=True, show_fig=False)
 
                             graph = TPsGraph(cm.tps_units)
@@ -191,3 +222,4 @@ if __name__ == "__main__":
                             with open(fi_dir + "classes.json", "w") as of:
                                 json.dump(list(cl_form), of)
                             # graph.get_communities()
+    print("END..")