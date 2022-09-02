eb62f0893ef12165f03f6b9a2a3c1e2de5ff655b
mat barb <mattia.barbaresi@gmail.com>
2022-09-02 15:33:24 +0200

    added complexity res and minors in main

--- creativitips/main.py
+++ creativitips/main.py
@@ -11,7 +11,7 @@ rng = np.random.default_rng(const.RND_SEED)
 # file_names = ["input", "input2", "saffran", "thompson_newport", "reber", "all_songs_in_G",
 #               "all_irish-notes_and_durations", "cello", "bach_compact"]
 
-file_names = ["mediapipe"]
+file_names = ["in"]
 
 # maintaining INTERFERENCES/FORGETS separation by a factor of 10
 interferences = [0.005]
@@ -26,22 +26,6 @@ for file_name in file_names:
             for interf in interferences:
                 for forg in forgets:
                     for t_mem in thresholds_mem:
-                        # # init
-                        # root_dir = const.OUT_DIR + "{}_{}/".format(method, time.strftime("%Y%m%d-%H%M%S"))
-                        # os.makedirs(root_dir, exist_ok=True)
-                        # with open(root_dir + "params.txt", "w") as of:
-                        #     json.dump({
-                        #         "rnd": const.RND_SEED,
-                        #         "w": const.WEIGHT,
-                        #         "interference": interf,
-                        #         "forgetting": forg,
-                        #         "mem thresh": t_mem,
-                        #         "lens": const.ULENS,
-                        #         "tps_order": order,
-                        #     }, of)
-                        #
-                        # print("processing {} series ...".format(file_name))
-
                         # read input model
                         ipath = const.OUT_DIR + "tps_results/" + \
                                 utils.params_to_string(method, order, forg, interf, t_mem) \
@@ -52,11 +36,12 @@ for file_name in file_names:
                         G = nx.DiGraph(read_dot(ipath))
                         # with open("data/" + file_name + ".txt", "r") as fp:
                         #     rep = fp.readlines()
-                        rep = ["DGGABABdBADFEDADFEDDGGABABdBADFEDADFEDDGGABABdBADFEDGGEDGGABABdBADFEDGGEGGBdgfefgfedBcBABcdefgfefgfedBcAFAGGBdGGBdgfefgfedBcBABcdefgfefgfedBcAFAGGBd"]
+                        # rep = ["DGGABABdBADFEDADFEDDGGABABdBADFEDADFEDDGGABABdBADFEDGGEDGGABABdBADFEDGGEGGBdgfefgfedBcBABcdefgfefgfedBcAFAGGBdGGBdgfefgfedBcBABcdefgfefgfedBcAFAGGBd"]
                         print("generating... ")
                         for _i in range(0, 100):
                             gens = ct.creative_gens(rng, G, n_seq=10, min_len=100)
-                            g_evals = ct.evaluate_similarity(gens, rep)  # prova
+                            # g_evals = ct.evaluate_similarity(gens, rep)  # prova
+                            g_evals = ct.evaluate_online(gens)
                             print("generated: ", g_evals)
                             G = ct.update(g_evals, G)
                         utils.plot_nx_creativity(G, "data/out/creativeGens")
