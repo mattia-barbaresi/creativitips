8fa58abeb802a46d83e7494e8f23d097e30fc0d4
mat barb <mattia.barbaresi@gmail.com>
2022-10-03 12:24:46 +0200

    minor

--- creativitips/tps_main.py
+++ creativitips/tps_main.py
@@ -59,7 +59,7 @@ if __name__ == "__main__":
     np.set_printoptions(linewidth=np.inf)
     rng = np.random.default_rng(const.RND_SEED)
 
-    file_names = ["input", "input2", "saffran", "thompson_newport",
+    file_names = ["input", "input2", "saffran", "thompson_newport", "reber",
                   "Onnis2003_L1_2","Onnis2003_L2_2","Onnis2003_L1_6","Onnis2003_L2_6",
                   "Onnis2003_L1_12","Onnis2003_L2_12","Onnis2003_L1_24","Onnis2003_L2_24",
                   "all_songs_in_G", "all_irish-notes_and_durations-abc", "bach_preludes", "ocarolan", "scottish"]
