ff7698bfe50c0db8da14eb3a831d68ed32270836
mat barb <mattia.barbaresi@gmail.com>
2022-11-04 16:13:17 +0100

    added kl utility in and cleaned metrics

--- creativitips/metrics/utility.py
+++ creativitips/metrics/utility.py
@@ -1,22 +1,32 @@
 #!/usr/bin/env python
-
 import sys
-from metrics import interval_functions as intfun
-import numpy as np
 import math
+import numpy as np
+from scipy.special import rel_entr
+from metrics import interval_functions as intfun
 
 
-#############################################################################################
 def utility(melody, allowed_intervals):
     # Input: melody (with accidentals) and array of allowed intervals
-    mybins = np.arange(-12, 12)
+    mybins = np.arange(-12, 13)
     rhist = np.histogram(allowed_intervals, bins=mybins, density=True)
     intervals = intfun.compute_intervals(melody)
     interval_hist = np.histogram(intervals, bins=mybins, density=True)
     a = np.array(rhist[0])
     b = np.array(interval_hist[0])
-    u = 1.0 / (math.dist(a, b) + 1)
-    return u
+    return 1.0 / (math.dist(a, b) + 1)
+
+
+def utility_kl(melody, allowed_intervals):
+    # Input: melody (with accidentals) and array of allowed intervals
+    mybins = np.arange(-12, 13)
+    rhist = np.histogram(allowed_intervals, bins=mybins, density=True)
+    intervals = intfun.compute_intervals(melody)
+    interval_hist = np.histogram(intervals, bins=mybins, density=True)
+    Q = np.array(rhist[0])
+    P = np.array(interval_hist[0])
+    kl = sum(rel_entr(P, Q))
+    return 1.0 / (kl + 1)
 
 
 if __name__ == "__main__":
@@ -27,5 +37,7 @@ if __name__ == "__main__":
     mel = intfun.key2accidentals(line, 'G')  # Hp: key of G
     ints = [4, 3, -3, -4, 9, -9]  # only thirds and sixths
     u = utility(mel, ints)
+    ukl = utility_kl(mel, ints)
 
     print(u)
+    print(ukl)
