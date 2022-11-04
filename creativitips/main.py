dc7f5595a7a9361f50d25c28dc1a38b3b04dcdda
mat barb <mattia.barbaresi@gmail.com>
2022-11-04 21:58:30 +0100

    evaluate_interval_function

--- creativitips/main.py
+++ creativitips/main.py
@@ -1,5 +1,7 @@
+import json
+import os
+
 import networkx as nx
-from networkx.drawing.nx_pydot import read_dot
 import const
 import utils
 import numpy as np
@@ -11,14 +13,14 @@ rng = np.random.default_rng(const.RND_SEED)
 # file_names = ["input", "input2", "saffran", "thompson_newport", "reber", "all_songs_in_G",
 #               "all_irish-notes_and_durations", "cello", "bach_compact"]
 
-file_names = ["input"]
+file_names = ["all_irish-notes_and_durations-abc"]
 
 # maintaining INTERFERENCES/FORGETS separation by a factor of 10
 interferences = [0.005]
 forgets = [0.05]
 thresholds_mem = [1.0]
 tps_orders = [2]
-methods = ["CT"]
+methods = ["FTP_WFWI"]
 
 for file_name in file_names:
     for method in methods:
@@ -27,22 +29,42 @@ for file_name in file_names:
                 for forg in forgets:
                     for t_mem in thresholds_mem:
                         # read input model
-                        ipath = const.OUT_DIR + "tps_results/" + \
-                                utils.params_to_string(method, order, forg, interf, t_mem) \
-                                + file_name + "/tps_units.dot"
-
+                        in_path = const.OUT_DIR + "tps_results_" + str(const.STM_DECAY_RATE) + "_" + str(const.LTM_DECAY_RATE) \
+                                  + "/" + utils.params_to_string(method, order, forg, interf, t_mem) \
+                                  + file_name + "/tps_units.dot"
+                        in_gpath = const.OUT_DIR + "tps_results_" + str(const.STM_DECAY_RATE) + "_" + str(const.LTM_DECAY_RATE) \
+                                   + "/" + utils.params_to_string(method, order, forg, interf, t_mem) \
+                                   + file_name + "/ggraph.dot"
                         # ============= GRAPH
                         # create graph from input model
-                        G = nx.DiGraph(read_dot(ipath))
+                        G = nx.DiGraph(nx.nx_pydot.read_dot(in_path))
+                        GG = nx.DiGraph(nx.nx_pydot.read_dot(in_gpath))
                         # with open("data/" + file_name + ".txt", "r") as fp:
                         #     rep = fp.readlines()
-                        # rep = ["DGGABABdBADFEDADFEDDGGABABdBADFEDADFEDDGGABABdBADFEDGGEDGGABABdBADFEDGGEGGBdgfefgfedBcBABcdefgfefgfedBcAFAGGBdGGBdgfefgfedBcBABcdefgfefgfedBcAFAGGBd"]
+                        gens_data = dict()
                         print("generating... ")
-                        for _i in range(0, 100):
-                            gens = ct.creative_gens(rng, G, n_seq=10, min_len=100)
+                        for _i in range(0, 50):
+                            # GENERATE
+                            # gens = ct.creative_gens(rng, G, n_seq=10, min_len=100)
+                            gens, gens_id = ct.creative_ggens(rng, GG, n_seq=50, min_len=50)
                             # g_evals = ct.evaluate_similarity(gens, rep)  # prova
-                            g_evals = ct.evaluate_online(gens)
-                            print("generated: ", g_evals)
-                            G = ct.update(g_evals, G)
-                        utils.plot_nx_creativity(G, "data/out/creativeGens")
-                        print("G edges: ", G.edges(data=True))
+                            # EVALUATE
+                            # g_evals = ct.evaluate_online(gens)
+                            g_evals = ct.evaluate_interval_function(gens)
+                            # G = ct.update(g_evals, G)
+                            GG = ct.gupdate(GG, g_evals, gens_id)
+                            if _i % 10 == 0:
+                                gens_data[_i] = {}
+                                gens_data[_i]["gens"] = gens
+                                gens_data[_i]["ids"] = gens_id
+
+                        os.makedirs(const.OUT_DIR + "/creativeGens/", exist_ok=True)
+                        ct.plot_nx_creativity(GG, const.OUT_DIR + "creativeGens/graph")
+                        gens_out, goid = ct.creative_ggens(rng, GG, n_seq=10, min_len=100)
+                        with open(const.OUT_DIR + "creativeGens/generated.json", "w") as fp:
+                            json.dump(gens_out, fp)
+                        with open(const.OUT_DIR + "creativeGens/gens_data.json", "w") as fp:
+                            json.dump(gens_data, fp)
+
+                        print("GG edges: ", GG.edges(data=True))
+print("END")
