a7d1ee0ee2fdb2556d69a9d923c832782e23f8ea
mat barb <mattia.barbaresi@gmail.com>
2022-11-25 13:56:29 +0100

    fixing shallow_parser

--- creativitips/misc/shallow_parser.py
+++ creativitips/misc/shallow_parser.py
@@ -2,26 +2,28 @@ import os
 from ccg_nlpy import local_pipeline
 
 pipeline = local_pipeline.LocalPipeline()
-rootdir = '../data/CHILDES_converted/'
+root_dir = '../data/CHILDES_converted/'
 
-for subdir, dirs, files in os.walk(rootdir):
-    if "Edinburgh" in subdir:
-        continue
+for subdir, dirs, files in os.walk(root_dir):
     for file in files:
-        if ('.capp' in file):
+        if '.capp' in file:
             filenm = file
             textfile = subdir + '/' + file
             outdir = subdir.replace("data", "data/out")
             os.makedirs(outdir, exist_ok=True)
 
+            print("processing file: ", filenm)
             with open(textfile, "r") as fpi:
                 with open(outdir + '/' + filenm.split('.capp')[0] + '.shpar', "w") as fpo:
                     for line in fpi.readlines():
-                        doc = pipeline.doc(line, pretokenized=False)
-                        # tks = list(x["tokens"] for x in doc.get_shallow_parse.cons_list)
+                        utter = line.split()
+                        # if list has less than 3 elements, it is empty b/c
+                        # auto-cleaning removed a non-speech sound, etc.
+                        if len(utter) < 3:
+                            continue
+                        doc = pipeline.doc(" ".join(utter[1:]), pretokenized=False)
                         tks = " || ".join(x["tokens"] for x in doc.get_shallow_parse.cons_list)
                         fpo.write(tks + "\n")
-
 
 # doc = pipeline.doc("The dog chase the cat", pretokenized=False)
 # sp = list(x["tokens"] for x in doc.get_shallow_parse.cons_list)
@@ -33,4 +35,3 @@ for subdir, dirs, files in os.walk(rootdir):
 # doc = pipeline.doc("I am gonna stop the train with my whist")
 # sp = " || ".join(x["tokens"] for x in doc.get_shallow_parse.cons_list)
 # print(sp)
-
