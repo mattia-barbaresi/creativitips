2fa885b630ed0dcd8f212e62a1738b7a4ea5a9fa
mat barb <mattia.barbaresi@gmail.com>
2022-11-25 13:11:49 +0100

    const, measures, shallow_parser, childes_converter

--- creativitips/misc/childes_converter.py
+++ creativitips/misc/childes_converter.py
@@ -1,10 +1,12 @@
 import os
 import string
+import re
+import const
 
-rootdir = "./data/Anne/"
-outdir = "./data/Anne_converted/"
-
+rootdir = const.CHILDES_REPO
+outdir = "../data/CHILDES_converted/"
 punctuation = string.punctuation
+
 for subdir, dirs, files in os.walk(rootdir):
     for file in files:
         if '.cha' in file:
@@ -18,8 +20,23 @@ for subdir, dirs, files in os.walk(rootdir):
             new_lines = []
             for line in lines:
                 if line.startswith("*"):
-                    new_lines.append("*"+line.translate(str.maketrans('', '', punctuation)).replace("	"," "))
+                    # get rid of special chars!!
+                    # if lpt != "." or lpt != "?":
+                    #     print("sdsdsfds: ", lpt)
+                    ss = " ".join(re.sub(r'\[[^()]*\]', '', line).split())
+                    ss = " ".join(re.sub(r'\[^()]*\', '', ss).split())
+                    pcs = ss.replace("„","").replace("+...","").replace("‡","").replace("_"," ")\
+                        .replace("xxx","").replace("+//","").replace("+/","").split()
+                    ss = " ".join(pcs[1:-1])
+
+                    nl = ss.translate(str.maketrans('', '', punctuation))\
+                        .replace("	"," ").strip()
+                    if len(nl.strip()) > 0:
+                        new_lines.append(" ".join([pcs[0], nl, pcs[-1]]) + "\n")
 
             if new_lines:
-                with open(outdir + new_file,"w") as fp:
+                vals = "/".join(subdir.split("/")[-1].split("\\"))
+                fout = outdir + vals
+                os.makedirs(fout, exist_ok=True)
+                with open(fout + "/" + new_file,"w") as fp:
                     fp.writelines(new_lines)
--- creativitips/misc/shallow_parser.py
+++ creativitips/misc/shallow_parser.py
@@ -4,23 +4,23 @@ pipeline = local_pipeline.LocalPipeline()
 file_in = "../data/br_text.txt"
 file_out = "../data/out/br_text.txt"
 
-with open(file_in, "r") as fpi:
-    with open(file_out, "w") as fpo:
-        for line in fpi.readlines():
-            doc = pipeline.doc(line, pretokenized=False)
-            # tks = list(x["tokens"] for x in doc.get_shallow_parse.cons_list)
-            tks = " || ".join(x["tokens"] for x in doc.get_shallow_parse.cons_list)
-            fpo.write(tks + "\n")
+# with open(file_in, "r") as fpi:
+#     with open(file_out, "w") as fpo:
+#         for line in fpi.readlines():
+#             doc = pipeline.doc(line, pretokenized=False)
+#             # tks = list(x["tokens"] for x in doc.get_shallow_parse.cons_list)
+#             tks = " || ".join(x["tokens"] for x in doc.get_shallow_parse.cons_list)
+#             fpo.write(tks + "\n")
 
 
 # doc = pipeline.doc("The dog chase the cat", pretokenized=False)
 # sp = list(x["tokens"] for x in doc.get_shallow_parse.cons_list)
 # print(sp)
 
-# from ccg_nlpy import remote_pipeline
-# 
-# pipeline = remote_pipeline.RemotePipeline()
-# doc = pipeline.doc("the naughty dog chased the cat alone")
-# sp = " || ".join(x["tokens"] for x in doc.get_shallow_parse.cons_list)
-# print(sp)
+from ccg_nlpy import remote_pipeline
+
+pipeline = remote_pipeline.RemotePipeline()
+doc = pipeline.doc("I am gonna stop the train with my whist")
+sp = " || ".join(x["tokens"] for x in doc.get_shallow_parse.cons_list)
+print(sp)
 
