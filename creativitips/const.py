be602d388aeb51c14704746678a24f9ec29c40d7
mat barb <mattia.barbaresi@gmail.com>
2023-01-17 15:47:50 +0100

    const

--- creativitips/const.py
+++ creativitips/const.py
@@ -2,13 +2,13 @@
 RND_SEED = 77
 MEM_THRES = 1.0
 WEIGHT = 1.0
-FORGETTING = 0.05
-INTERFERENCE = 0.005
-ULENS = [1]
-TPS_ORDER = 1
+INTERFERENCE = 0.001
+ULENS = [2,3]
+TPS_ORDER = 2
 OUT_DIR = "data/out/"
 EXAMPLES_OUT_DIR = "./out/"
 GRAPH_SEP = " | "
+# with 1000 decay is constant, with 1 decay go to 0 very fast
 STM_DECAY_RATE = 1000
-LTM_DECAY_RATE = 100
+LTM_DECAY_RATE = 10
 CHILDES_REPO = "C:/Users/matti/OneDrive/Desktop/DOTT/CHILDES/"
